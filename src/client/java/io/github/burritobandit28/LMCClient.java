aea3eaefe21f99fb697d3ac65968ff4953d2707a
BurritoBandit28 <yansalmon@gmail.com>
2024-10-14 23:56:48 +0100

    fixed transparency

--- src/client/java/io/github/burritobandit28/LMCClient.java
+++ src/client/java/io/github/burritobandit28/LMCClient.java
@@ -1,10 +1,14 @@
 package io.github.burritobandit28;
 
+import io.github.burritobandit28.lmc.blocks.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 public class LMCClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.IO_BLOCK, RenderLayer.getCutout());
     }
 }
